53b8c20f6433ee2bdb0e18e43cc157cff80a182d
Roman Medvedev <r.medvedev@parad-shoes.ru>
2021-02-13 23:52:50 +0300

    changed path of bundle. separated functions. renamed variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,95 +1,97 @@
 <script>
- import { onMount } from "svelte";
-	export let text = "";
-	export let name = "";
-	let page = 0;
-	let pageSize = 1;
-	let json= {};
-	let totalpage = 0;
-	
-	onMount(function(){
-	thispage(page)}
-	);
+  import { onMount } from "svelte";
+  export let name = "";
+  export let recivedData = {};
+  let currentPage = 0;
+  let pageSize = 1;
+  let totalPage = 1;
+  let url = "http://localhost:3000/list/";
+
+  onMount(function() {
+    getPage(currentPage);
+  });
 
   function nextpage() {
-    page += 1;
-	thispage(page);
+    currentPage += 1;
+    getPage(currentPage);
   }
 
   function prevpage() {
-    page -= 1;
-	thispage(page);
+    currentPage -= 1;
+    getPage(currentPage);
   }
-  function thispage(x) {
-    page = x;
-	let query = [];
-	let url = 'http://localhost:3000/list/';
-	if (page > 0){
-		query.push('page=' + page);
-	}
-	if (pageSize > 1){
-		query.push('pageSize=' + pageSize);
-	}
-	if (query.length > 0){
-		url = url + '?' + query.join('&');
-	}
-	getFetch(url).then(res => {
-	json = res;
-	text = json.text;
-	totalpage = json.settings.totalpage;
-	if (totalpage - 1 < page){
-		page = totalpage - 1;
-		thispage(page);
-	}
-	});
+
+  function getPage(x) {
+    currentPage = x;
+    let urlToSend = url + dataToSend();
+    getFetch(urlToSend).then((res) => {
+      recivedData = res;
+      totalPage = recivedData.settings.totalPage;
+      if (totalPage - 1 < currentPage) {
+        getPage(totalPage - 1);
+      }
+    });
   }
+
+  function dataToSend() {
+    let query = [];
+    if (currentPage > 0) {
+      query.push("page=" + currentPage);
+    }
+    if (pageSize > 1) {
+      query.push("pageSize=" + pageSize);
+    }
+    if (query.length > 0) {
+      return "?" + query.join("&");
+    }
+    return "";
+  }
+
   async function getFetch(url) {
-    let response = await fetch(url);
-	let resp = await response.json();
-    return resp;
-	}
+    return await fetch(url).then((res) => {
+      return res.json();
+    });
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>{text}</p>
-	<div>Текущея страница: {page + 1}</div>
-	<div>
-		{#each  Array(totalpage) as _, i}
-			<button on:click={() => thispage(i)}>page{ i + 1 }</button>
-		{/each}
-	</div>
-	<div>
-		{#if page > 0}
-			<button on:click={prevpage}>prevpage</button>
-		{/if}
-		{#each  Array(page) as _, i}
-			<button on:click={() => thispage(i)}>page{ i + 1 }</button>
-		{/each}
-		<button on:click={nextpage}>nextpage</button>
-	</div>
-
-
+  <h1>Hello {name}!</h1>
+  <p>{recivedData.text}</p>
+  <div>Текущея страница: {currentPage + 1}</div>
+  <div>
+    {#each Array(totalPage) as _, i}
+      <button on:click={() => getPage(i)}>page{i + 1}</button>
+    {/each}
+  </div>
+  <div>
+    {#if currentPage > 0}
+      <button on:click={prevpage}>prevpage</button>
+    {/if}
+    {#each Array(currentPage) as _, i}
+      <button on:click={() => getPage(i)}>page{i + 1}</button>
+    {/each}
+    <button on:click={nextpage}>nextpage</button>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
